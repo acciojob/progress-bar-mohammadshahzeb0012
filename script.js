6ff2e594d4c26f9ac9d29846cc24946f1a22b18d
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-17 13:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+  <script>
+      let currentNode = 1;
+      const prev = document.getElementById("prev");
+      const next = document.getElementById("next");
+
+   function navigate(diresction){
+    const currentElement = document.getElementById(currentNode);
+   if(diresction === 'next' && currentElement.nextElementSibling){
+    currentElement.nextElementSibling.classList.add('active')
+    currentElement.nextElementSibling.nextElementSibling.classList.add('active')
+    currentNode++
+   }else if(diresction === 'prev' && currentElement.previousElementSibling){
+    currentElement.classList.remove('active')
+    currentElement.previousElementSibling.classList.remove('active')
+    currentNode--
+   }
+   currentNode === 5 ? (next.disabled = true) : (next.disabled = false)
+   currentNode === 1 ?  (prev.disabled = true) : (prev.disabled = false)
+   } 
+
+   prev.addEventListener('click',()=>{
+    navigate('prev')
+   })
+   next.addEventListener('click',()=>{
+     navigate('next')
+   })
+
+  </script>
+
